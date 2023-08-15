048a81a1a4ac8906ce136b9a4eae6cf0cc10d8fb
Macarena <macarenacastilloruiz@gmail.com>
2023-08-15 17:38:54 -0300

    Second commit

--- front-end/src/redux/action-types.js
+++ front-end/src/redux/action-types.js
@@ -1,48 +1,48 @@
 // ! USERS
-export const LOGIN = "LOGIN"; //OK
-export const REGISTER_USER = "REGISTER_USER"; //OK
-export const USER_PROFILE = "USER_PROFILE"; //OK
-export const UPDATE_USER = "UPDATE_USER"; //OK
-export const GET_MY_RECIPES = "GET_MY_RECIPES"; //OK
+export const LOGIN = "LOGIN"; // ? OK
+export const REGISTER_USER = "REGISTER_USER"; // ? OK
+export const USER_PROFILE = "USER_PROFILE"; // ? OK
+export const UPDATE_USER = "UPDATE_USER"; // ? OK
+export const GET_MY_RECIPES = "GET_MY_RECIPES"; // ? OK
 
 // ! RECIPES
-export const GET_ALL_RECIPES = "GET_ALL_RECIPES"; //OK
-export const GET_RECIPE_BY_NAME = "GET_RECIPE_BY_NAME"; //OK
-export const GET_RECIPE_BY_ID = "GET_RECIPE_BY_ID"; //OK
-export const CREATE_RECIPE = "CREATE_RECIPE"; //OK
-export const UPDATE_RECIPE = "UPDATE_RECIPE"; //OK
-export const DELETE_RECIPE = "DELETE_RECIPE";  //OK
-export const GET_DIETS_OF_ONE_RECIPE = "GET_DIETS_OF_ONE_RECIPE"  //OK
+export const GET_ALL_RECIPES = "GET_ALL_RECIPES"; // ? OK
+export const GET_RECIPE_BY_NAME = "GET_RECIPE_BY_NAME"; // ? OK
+export const GET_RECIPE_BY_ID = "GET_RECIPE_BY_ID"; // ? OK
+export const CREATE_RECIPE = "CREATE_RECIPE"; // ? OK
+export const UPDATE_RECIPE = "UPDATE_RECIPE"; // ? OK
+export const DELETE_RECIPE = "DELETE_RECIPE";  // ? OK
+export const GET_DIETS_OF_ONE_RECIPE = "GET_DIETS_OF_ONE_RECIPE"  // ? OK
 
 // ? Ordenamientos Recipes
-export const SORT_RECIPES_TITLE = "SORT_RECIPES_TITLE"; //OK
-export const SORT_RECIPES_HEALTH_SCORE = "SORT_RECIPES_HEALTH_SCORE"; //OK
-export const SORT_BY_PRICE = "SORT_BY_PRICE"; //OK
+export const SORT_RECIPES_TITLE = "SORT_RECIPES_TITLE"; // ? OK
+export const SORT_RECIPES_HEALTH_SCORE = "SORT_RECIPES_HEALTH_SCORE"; // ? OK
+export const SORT_BY_PRICE = "SORT_BY_PRICE"; // ? OK
 
 // ? Filters Recipes
-export const FILTER_BY_VEGAN = "FILTER_BY_VEGAN"; //OK
-export const FILTER_BY_POPULAR = "FILTER_BY_POPULAR"; //OK
-export const FILTER_BY_VEGETARIAN = "FILTER_BY_VEGETARIAN"; //OK
-export const FILTER_BY_GLUTEN_FREE = "FILTER_BY_GLUTEN_FREE"; //OK
+export const FILTER_BY_VEGAN = "FILTER_BY_VEGAN"; // ? OK
+export const FILTER_BY_POPULAR = "FILTER_BY_POPULAR"; // ? OK
+export const FILTER_BY_VEGETARIAN = "FILTER_BY_VEGETARIAN"; // ? OK
+export const FILTER_BY_GLUTEN_FREE = "FILTER_BY_GLUTEN_FREE"; // ? OK
 
 // ! DIETS
-export const GET_ALL_DIETS = "GET_ALL_DIETS"; //OK
-export const GET_DIET_BY_NAME = "GET_DIET_BY_NAME"; //OK
-export const GET_DIET_BY_ID = "GET_DIET_BY_ID"; //OK
-export const DELETE_DIET = "DELETE_DIET";  //OK
-export const DELETE_DIET_OF_ONE_RECIPE = "DELETE_DIET_OF_ONE_RECIPE" //OK
-export const CREATE_DIET = "CREATE_DIET"  //OK
-export const GET_MY_DIETS = "GET_MY_DIETS"  //OK
-export const UPDATE_DIET = "UPDATE_DIET"    //
+export const GET_ALL_DIETS = "GET_ALL_DIETS"; // ? OK
+export const GET_DIET_BY_NAME = "GET_DIET_BY_NAME"; // ? OK
+export const GET_DIET_BY_ID = "GET_DIET_BY_ID"; // ? OK
+export const DELETE_DIET = "DELETE_DIET";  // ? OK
+export const DELETE_DIET_OF_ONE_RECIPE = "DELETE_DIET_OF_ONE_RECIPE" // ? OK
+export const CREATE_DIET = "CREATE_DIET"  // ? OK
+export const GET_MY_DIETS = "GET_MY_DIETS"  // ? OK
+export const UPDATE_DIET = "UPDATE_DIET"    // ? OK
 
 // ! FAVORITES
-export const ADD_FAVORITE = "ADD_FAVORITE";  //OK
-export const GET_ALL_FAVORITES = "GET_ALL_FAVORITES";  //OK
-export const DELETE_FAVORITE = "DELETE_FAVORITE";  //OK
+export const ADD_FAVORITE = "ADD_FAVORITE";  // ? OK
+export const GET_ALL_FAVORITES = "GET_ALL_FAVORITES";  // ? OK
+export const DELETE_FAVORITE = "DELETE_FAVORITE";  // ? OK
 
 // ! CLEAN Y RESET
-export const RESET = "RESET"; //OK
-export const CLEAN = "CLEAN"; //OK
+export const RESET = "RESET"; // ? OK
+export const CLEAN = "CLEAN"; // ? OK
 
 
 
